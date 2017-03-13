fd2c7a69d3c29c5ed44e7836e3f397d3f9a9cdb3
jmacnicol <jmacnicol1020@gmail.com>
2017-03-13 17:05:25 -0600

    No longer needed after AsyncTaskLoader refactor

--- app/src/main/java/com/macnicol/popularmoviesproject/AsyncTaskCompleteListener.java
+++
@@ -1,6 +0,0 @@
-package com.macnicol.popularmoviesproject;
-
-public interface AsyncTaskCompleteListener {
-    void onFetchMovieTaskPreExecute();
-    void onFetchMovieTaskComplete(MovieItem[] movieItems);
-}
